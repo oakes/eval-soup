2a7941bcfdccac8f09f91d8854783df0c50be199
oakes <zsoakes@gmail.com>
2016-09-28 12:55:56 -0400

    Initial import

---
+++ build.boot
@@ -0,0 +1,26 @@
+(set-env!
+  :source-paths #{"src"}
+  :resource-paths #{"src" "resources"}
+  :dependencies '[[org.clojure/clojure "1.8.0"]
+                  [org.clojure/clojurescript "1.9.225"]
+                  [org.clojure/core.async "0.2.374"]
+                  [clojail "1.0.6"]]
+  :repositories (conj (get-env :repositories)
+                  ["clojars" {:url "https://clojars.org/repo/"
+                              :username (System/getenv "CLOJARS_USER")
+                              :password (System/getenv "CLOJARS_PASS")}]))
+
+(task-options!
+  pom {:project 'eval-soup
+       :version "1.0.0-SNAPSHOT"
+       :description "A nice eval wrapper for Clojure and ClojureScript"
+       :url "https://github.com/oakes/eval-soup"
+       :license {"Public Domain" "http://unlicense.org/UNLICENSE"}}
+  push {:repo "clojars"})
+
+(deftask local []
+  (comp (pom) (jar) (install)))
+
+(deftask deploy []
+  (comp (pom) (jar) (push)))
+
---
+++ src/eval_soup/core.clj
@@ -0,0 +1,46 @@
+(ns eval-soup.core
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]
+            [clojail.core :as clojail])
+  (:import [java.io File StringWriter]))
+
+(defmacro with-security [& body]
+  `(do
+     (System/setProperty "java.security.policy"
+                        (-> "java.policy" io/resource .toString))
+     (System/setSecurityManager
+       (proxy [SecurityManager] []
+         (checkExit [status#]
+           (throw (SecurityException. "Exit not allowed.")))))
+     (try ~@body
+       (finally (System/setSecurityManager nil)))))
+
+(defn eval-form-safely [form nspace]
+  (with-security
+    (clojail/thunk-timeout
+      (fn []
+        (binding [*ns* nspace
+                  *out* (StringWriter.)]
+          (refer-clojure)
+          [(eval form)
+           (if (and (coll? form) (= 'ns (first form)))
+             (-> form second create-ns)
+             *ns*)]))
+      1000)))
+
+(defn eval-form [form-str nspace]
+  (binding [*read-eval* false]
+    (try
+      (eval-form-safely (read-string form-str) nspace)
+      (catch Exception e [e nspace]))))
+
+(defn code->results [forms-str]
+  (loop [forms (edn/read-string forms-str)
+         results []
+         nspace (create-ns 'clj.user)]
+    (if-let [form (first forms)]
+      (let [[result current-ns] (eval-form form nspace)
+            result-str (if (instance? Exception result) [(.getMessage result)] (pr-str result))]
+        (recur (rest forms) (conj results result-str) current-ns))
+      results)))
+
